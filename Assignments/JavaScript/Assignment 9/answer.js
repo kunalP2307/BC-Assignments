d1e048155fcdf97e34cdcf6d99fbf95d521b98d5
kunalP2307 <kunalpatil2307@gmail.com>
2022-12-21 22:49:00 +0530

    Added code for JS assignment 9

---
+++ Assignments/JavaScript/Assignment 9/answer.js
@@ -0,0 +1,14 @@
+/**
+ * 1. Carefully observe this example.
+    a) Is the InnerFunction() a closure?
+ ->     Yes
+    b) What is output of this program
+ ->     100
+*/
+
+/** 
+ * Output of following closure ?
+ *-> 3
+     3
+     3
+*/ 
